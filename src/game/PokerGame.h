1d3e078723973d9bfbbbabce295ba056a299d8ae
Harry Guan <guanhuai2014@gmail.com>
2024-11-26 15:22:37 -0600

    removed format from workflow

---
+++ src/game/PokerGame.h
@@ -0,0 +1,15 @@
+pragma #once
+
+#include <Deck.h>
+#include <HandEvaluator.h>
+
+  class PokerGame
+{
+    // Need: constructor, playGame()
+    // folds will be through the actual actions of the gameplaying agents (but it should invoke removePlayer() from this
+    // class) has to have totalchips getters and setters (setter can just be increments since you cant take chips from
+    // the pot) has to have ascii art for each turn (maybe clear if possible)
+    //
+
+  public:
+}
